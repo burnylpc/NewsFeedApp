ef07f96ff680037ba7a03a36f6b2048ff1ac5297
burnylpc <burnylpc@gmail.com>
2021-10-21 21:01:03 +0500

    --

--- app/src/main/java/com/example/myapplication/di/Module.kt
+++ app/src/main/java/com/example/myapplication/di/Module.kt
@@ -1,7 +1,24 @@
 package com.example.newsfeed.di
 
+import okhttp3.OkHttpClient
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val BASE_URL = "https://newsapi.org/"
 
 val appModule = module {
+
+    single<OkHttpClient> {
+        OkHttpClient.Builder().build()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+    }
 
 }
